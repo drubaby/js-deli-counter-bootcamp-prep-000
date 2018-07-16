4efcfada5a5bb8c8738f80180441bef596962772
Dru Edmondson <peryton6@gmail.com>
2018-07-16 02:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function currentLine(katzDeli){
     return "The line is currently empty."
   }
   else {
-    for (let i = 1; i < katzDeli.length; i++){
-      theLineArray += i + ". " + katzDeli[i] + ", "
+    for (let i = 0; i < katzDeli.length; i++){
+      theLineArray += (i + 1) ". " + katzDeli[i] + ", "
     }
       var finalLine = theLineArray.slice(0, theLineArray.length - 2)
       return `The line is currently: ` + finalLine
